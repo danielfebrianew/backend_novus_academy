4723ff50d50ed9a7d1176a5e70a08da5936fe6e9
Daniel Febrian <73058494+danielfebrianew@users.noreply.github.com>
2026-01-24 11:17:43 +0700

    added local config

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://member.novusnextgen.com'],
+    origin: ['http://localhost:3001', 'http://localhost:3000', 'https://member.novusnextgen.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
